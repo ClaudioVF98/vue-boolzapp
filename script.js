079d34c2599b38d96b5d48635e4dd1d60d3f6705
Claudio <claudiovarrosi@gmail.com>
2022-05-30 17:15:06 +0200

    Contatti stampati con V-for

---
+++ script.js
@@ -0,0 +1,170 @@
+const contacts = new Vue({
+    el : "#app",
+    data : {
+        currentContact : 0,
+        contacts : [
+            {
+                name: 'Michele',
+                avatar: 'img/avatar2.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Giapiero',
+                avatar: 'img/avatar3.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Lella',
+                avatar: 'img/avatar4.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Rubino',
+                avatar: 'img/avatar5.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Gianni',
+                avatar: 'img/avatar6.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Loredana',
+                avatar: 'img/avatar7.png',
+                visible: true,
+                messages: [
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'sent'
+                    },
+                    {
+                        time: '15:30',
+                        message: 'Ciao',
+                        status: 'received'
+                    }
+                ]
+            }
+        ]       
+    }
+})
